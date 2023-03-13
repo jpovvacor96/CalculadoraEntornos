aebacc8a3fdf2ab6a34ee55dc8bd3724ed0465bd
Kin00VP <127196891+Kin00VP@users.noreply.github.com>
2023-03-13 08:34:29 +0100

    Create Caracola.java
    programa de prueba

---
+++ hola/Caracola.java
@@ -0,0 +1,11 @@
+package default;
+
+public class Caracola {
+
+  public static void main(String[] args) {
+    
+    System.out.println("ESTO ES UNA PRUEBA DE UPLOADING EN CARPETA 
+                       + "INTERNA NO CREADA PREVIAMENTE");
+  }
+
+}
